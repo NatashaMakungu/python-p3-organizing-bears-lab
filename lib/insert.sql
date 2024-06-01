5706d616cdf9106f24a1e56a8716ac7733d41f33
Natasha Makungu <natasha.makungu@student.moringaschool.com>
2024-06-02 01:09:59 +0300

    complete lib

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+    ('Mr. Chocolate', 9, 'M', 'Brown', 'Calm', 1),
+    ('Rowdy', 7, 'M', 'Black', 'Aggressive', 1),
+    ('Tabitha', 4, 'F', 'Blonde', 'Playful', 1),
+    ('Sergeant Brown', 10, 'M', 'Brown', 'Stern', 1),
+    ('Melissa', 3, 'F', 'Brown', 'Gentle', 1),
+    ('Grinch', 6, 'M', 'Green', 'Grumpy', 1),
+    ('Wendy', 5, 'F', 'Black', 'Friendly', 1),
+    (NULL, 2, 'F', 'Brown', 'Shy', 1);
